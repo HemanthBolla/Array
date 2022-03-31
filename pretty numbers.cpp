53d261cdc934793942a2c722758036a49c08848f
SAI HEMANTH BOLLA <71115725+Sai1166@users.noreply.github.com>
2022-03-31 06:28:36 -0700

    Update pretty numbers.cpp

--- pretty numbers.cpp
+++ pretty numbers.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
-    int count=0;
+    int t;
     int l,r;
-    cin>>l>>r;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>l>>r;
+        int count=0;
 	for(int i=l;i<=r;i++)
 	{
 	    if(i%10==2||i%10==3||i%10==9)
@@ -13,6 +17,8 @@ int main() {
 	    }
 	   
 	}
-	cout<<count;
+	cout<<count<<"\n";
+    }
+	
 	return 0;
 }
